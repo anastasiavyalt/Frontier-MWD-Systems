f7655905de2714caf00ba0813b99fb790a7fe20f
Anastasia <anastasiavyalt@gmail.com>
2020-02-21 16:44:19 +0700

    Header changes color after first screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,19 @@
+/* Header */
+let browserHeight = document.documentElement.clientHeight;
+let header = document.querySelector(".header");
+let logo = document.querySelector(".header__logo");
+window.onscroll = () => {
+    let yPosition = window.pageYOffset;
+    if(yPosition > browserHeight) {
+        header.classList.add("header--light-theme");
+        logo.classList.add("header__logo--light-theme");
+    } else if(yPosition <= browserHeight) {
+        header.classList.remove("header--light-theme");
+        logo.classList.remove("header__logo--light-theme");
+    }
+}
+
+/* Swipers */
 let heroSwiper = new Swiper('.hero__swiper-container', {
     speed: 400,
     pagination: {
